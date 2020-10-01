8a2751730b8ab51cec2af9414f86718b0e65bc09
Josh Everett <josheverett93@gmail.com>
2020-10-01 17:37:08 -0500

    created CRUD routes

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,10 +1,46 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+# DBSession = sessionmaker(bind=engine)
+# session = DBSession()
+
+# @app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description 
+        output += '</br>'
+        output += '</br>'
+    return output
+
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a new menu item. Task 2 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/delete/')
+def deleteMenuItem(restaurant_id):
+    return "page to delete a new menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
